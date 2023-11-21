74b4214b63a6c3a81511a0d2e45d616ce9f75e82
LINNETWAHOME02 <nyokabilinnet@gmail.com>
2023-11-21 17:47:57 +0000

    Classes and objects

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Square module."""
+
+
+class Square:
+    """Definition of the square."""
+
+    def __init__(self, size):
+        """Constructor.
+        Arguments:
+            size: length of side of the square.
+        """
+        self.__size = size
