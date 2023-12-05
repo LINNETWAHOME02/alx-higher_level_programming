c4eca2b2580762e04120c672d727bb3e0f40f301
LINNETWAHOME02 <98949954+LINNETWAHOME02@users.noreply.github.com>
2023-12-05 22:27:37 +0300

    Python - Inheritance

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Defines an object attribute lookup function."""
+
+
 def lookup(obj):
-    """Returns a list of available attributes and methods of an object."""
-    return [attr for attr in dir(obj) if not callable(getattr(obj, attr)) or not attr.startswith("__")]
+    """Return a list of an object's available attributes."""
+    return (dir(obj))
